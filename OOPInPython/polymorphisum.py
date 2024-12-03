8e6387e05faaabf354eb0beed0080f42212a030a
HP <amxdhnanditha@gmail.com>
2024-12-03 13:59:29 +0530

    access modifiers in python

--- OOPInPython/polymorphisum.py
+++ OOPInPython/polymorphisum.py
@@ -1,1 +1,13 @@
 # Polymorphisum with python
+# It allows objects of different classes to be treated as objects of a common super class, and it enables you to write more
+# flexible and reusable codes
+
+
+# Encapsulation with python 
+
+# Access modifiers in python
+# class Main:
+#     name = '' #public
+#     _age = '' #protected (one ( _ ))
+#     __city = '' #private (two ( _ ))
+
