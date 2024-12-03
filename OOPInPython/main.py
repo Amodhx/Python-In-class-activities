7f81d3809af0f9a94cd96c8eb74091225f9bae0f
HP <amxdhnanditha@gmail.com>
2024-12-03 08:57:56 +0530

    defined class created obj and access it

---
+++ OOPInPython/main.py
@@ -0,0 +1,19 @@
+# Class ekak defined krddi first letter ek capital use krnn
+# e wageme camel case wlin define krnn
+# class attribute defined krddi simple start
+# Example
+
+class CarItem:
+    brand = ''
+    year = ''
+
+
+# How crete object 
+new_car = CarItem()
+
+# how access object item
+new_car.brand = 'AUDI'
+print(new_car.brand)
+
+
+
